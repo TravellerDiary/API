03022b1ac8886ba7b4ad9f99ce94e07bcf8c0e89
Jerry Wang <focaaby@gmail.com>
2016-05-17 21:40:25 +0800

    changed public dirname to views

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('less-middleware')(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 //===================connected to the MongoDB============================
