c213bec970bda026c62c87fd557666ea505de836
Jerry Wang <focaaby@gmail.com>
2016-05-17 21:19:56 +0800

    added ejs engine for html
    changed:
    	- comment unuse code in routes/index.js

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.use('/api',user);
 
 //==============view engine setup================================
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
